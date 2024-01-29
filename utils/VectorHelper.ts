9d691275d701b40ebeada936b7729c18f7578610
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2024-01-28 20:30:43 -0800

    Add dotproduct

--- utils/VectorHelper.ts
+++ utils/VectorHelper.ts
@@ -1,6 +1,9 @@
 import { Vector, Vector3 } from "@minecraft/server";
 
 export default class VectorHelper {
+	public static dotProduct = (a: Vector3, b: Vector3): number => {
+		return a.x * b.x + a.y * b.y + a.z * b.z;
+	};
 	public static normalize = (vector: Vector3) => {
 		return Vector.divide(vector, Vector.distance(vector, Vector.zero));
 	};
