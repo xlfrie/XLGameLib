54e751ba89549cc5c69b957e912ca493777e72c8
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2024-01-28 18:37:47 -0800

    Add particle helper

---
+++ utils/ParticleHelper.ts
@@ -0,0 +1,82 @@
+import { BoundingBox, Dimension, MolangVariableMap, Vector, Vector3 } from "@minecraft/server";
+import VectorHelper from "./VectorHelper";
+
+export default class ParticleHelper {
+	public static drawLine = (from: Vector3, to: Vector3, particle: string, dimension: Dimension, molang?: MolangVariableMap) => {
+		let dir = VectorHelper.normalize(Vector.subtract(to, from));
+		let squareLength = Vector.distance(from, to);
+		let inc = 0.5;
+		let increment = Vector.multiply(dir, inc);
+		let pos = from;
+		for (let i = 0; i * inc < squareLength; i++) {
+			dimension.spawnParticle(particle, pos, molang);
+
+			pos = Vector.add(pos, increment);
+		}
+	};
+
+	public static drawCuboid = (boundingBox: BoundingBox, particle: string, dimension: Dimension) => {
+		const diff = Vector.subtract(boundingBox.max, boundingBox.min);
+		this.drawLine(boundingBox.min, Vector.add(boundingBox.min, { x: diff.x, y: 0, z: 0 }), particle, dimension);
+		this.drawLine(
+			Vector.add(boundingBox.min, { x: 0, y: 0, z: diff.z }),
+			Vector.add(boundingBox.min, { x: diff.x, y: 0, z: diff.z }),
+			particle,
+			dimension
+		);
+
+		this.drawLine(boundingBox.min, Vector.add(boundingBox.min, { x: 0, y: 0, z: diff.z }), particle, dimension);
+		this.drawLine(
+			Vector.add(boundingBox.min, { x: diff.x, y: 0, z: 0 }),
+			Vector.add(boundingBox.min, { x: diff.x, y: 0, z: diff.z }),
+			particle,
+			dimension
+		);
+
+		this.drawLine(
+			Vector.add(boundingBox.min, { x: 0, y: diff.y, z: 0 }),
+			Vector.add(boundingBox.min, { x: diff.x, y: diff.y, z: 0 }),
+			particle,
+			dimension
+		);
+		this.drawLine(
+			Vector.add(boundingBox.min, { x: 0, y: diff.y, z: diff.z }),
+			Vector.add(boundingBox.min, { x: diff.x, y: diff.y, z: diff.z }),
+			particle,
+			dimension
+		);
+
+		this.drawLine(
+			Vector.add(boundingBox.min, { x: 0, y: diff.y, z: 0 }),
+			Vector.add(boundingBox.min, { x: 0, y: diff.y, z: diff.z }),
+			particle,
+			dimension
+		);
+		this.drawLine(
+			Vector.add(boundingBox.min, { x: diff.x, y: diff.y, z: 0 }),
+			Vector.add(boundingBox.min, { x: diff.x, y: diff.y, z: diff.z }),
+			particle,
+			dimension
+		);
+
+		this.drawLine(boundingBox.min, Vector.add(boundingBox.min, { x: 0, y: diff.y, z: 0 }), particle, dimension);
+		this.drawLine(
+			Vector.add(boundingBox.min, { x: diff.x, y: 0, z: 0 }),
+			Vector.add(boundingBox.min, { x: diff.x, y: diff.y, z: 0 }),
+			particle,
+			dimension
+		);
+		this.drawLine(
+			Vector.add(boundingBox.min, { x: 0, y: 0, z: diff.z }),
+			Vector.add(boundingBox.min, { x: 0, y: diff.y, z: diff.z }),
+			particle,
+			dimension
+		);
+		this.drawLine(
+			Vector.add(boundingBox.min, { x: diff.x, y: 0, z: diff.z }),
+			Vector.add(boundingBox.min, { x: diff.x, y: diff.y, z: diff.z }),
+			particle,
+			dimension
+		);
+	};
+}
