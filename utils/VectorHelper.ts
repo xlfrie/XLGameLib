dc80b88f55076aef612f6bd538b4ef3541d80a4b
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2024-02-19 14:21:32 -0800

    Add lerp function

--- utils/VectorHelper.ts
+++ utils/VectorHelper.ts
@@ -7,4 +7,11 @@ export default class VectorHelper {
 	public static normalize = (vector: Vector3) => {
 		return Vector.divide(vector, Vector.distance(vector, Vector.zero));
 	};
+	public static lerp = (a: Vector3, b: Vector3, t: number): Vector3 => {
+		let out = { x: 0, y: 0, z: 0 };
+		out.x = b.x * t + a.x * (1 - t);
+		out.y = b.y * t + a.y * (1 - t);
+		out.z = b.z * t + a.z * (1 - t);
+		return out;
+	};
 }
