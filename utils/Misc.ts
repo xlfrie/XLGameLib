62a48d3319a9b484550f2b51bb4d822c398783bd
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2024-02-19 15:10:22 -0800

    Add bounding box check function

--- utils/Misc.ts
+++ utils/Misc.ts
@@ -1,3 +1,15 @@
+import { BoundingBox, Vector3 } from "@minecraft/server";
+
 export default class Misc {
 	public static clamp = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max);
+	public static isInside = (box: BoundingBox, pos: Vector3): boolean => {
+		let x = box.max.x - box.min.x,
+			y = box.max.y - box.min.y,
+			z = box.max.z - box.min.z;
+		let x_ = box.max.x - pos.x,
+			y_ = box.max.y - pos.y,
+			z_ = box.max.z - pos.z;
+
+		return x >= x_ && y >= y_ && z >= z_;
+	};
 }
