18a4b62f72cb21699da861bff5b00393daca8d8d
XLFrie <xlfrys@gmail.com>
2024-12-07 12:31:19 -0800

    Didn't stage last file

--- managers/CommandManager.ts
+++ managers/CommandManager.ts
@@ -1,53 +1,53 @@
 import * as server from "@minecraft/server";
-import Command from "types/Command";
+import Command from "../types/Command";
 
 export default class CommandManager {
-	private static registeredCommands: Command[] = [];
-	private static config: CommandManagerConfigData;
+    private static registeredCommands: Command[] = [];
+    private static config: CommandManagerConfigData;
 
-	public static registerCommands = (commands: Command[]) => {
-		this.registeredCommands = this.registeredCommands.concat(
-			this.registeredCommands,
-			commands
-		);
-		return this;
-	};
+    public static registerCommands = (commands: Command[]) => {
+        this.registeredCommands = this.registeredCommands.concat(
+            this.registeredCommands,
+            commands
+        );
+        return this;
+    };
 
-	public static setConfig = (data: CommandManagerConfigData) => {
-		this.config = data;
-		return this;
-	};
+    public static setConfig = (data: CommandManagerConfigData) => {
+        this.config = data;
+        return this;
+    };
 
-	private static chatSendBefore = (event: server.ChatSendBeforeEvent) => {
-		let message = event.message;
-		if (!message.startsWith(this.config.prefix)) return;
+    private static chatSendBefore = (event: server.ChatSendBeforeEvent) => {
+        let message = event.message;
+        if (!message.startsWith(this.config.prefix)) return;
 
-		let commandName = message
-			.slice(this.config.prefix.length)
-			.split(" ")[0];
-		event.cancel = true;
+        let commandName = message
+            .slice(this.config.prefix.length)
+            .split(" ")[0];
+        event.cancel = true;
 
-		this.registeredCommands.forEach((command) => {
-			if (command.name == commandName) {
-				server.system.run(() => {
-					let status = command.execute(event);
-					switch (status) {
-						case -1:
-							event.sender.sendMessage("§cSomething went wrong");
-							break;
-					}
-				});
-			}
-		});
-	};
+        this.registeredCommands.forEach((command) => {
+            if (command.name == commandName) {
+                server.system.run(() => {
+                    let status = command.execute(event);
+                    switch (status) {
+                        case -1:
+                            event.sender.sendMessage("§cSomething went wrong");
+                            break;
+                    }
+                });
+            }
+        });
+    };
 
-	public static init = () => {
-		if (!this.config)
-			throw Error("Command Manager initialized before configured.");
-		server.world.beforeEvents.chatSend.subscribe(this.chatSendBefore);
-	};
+    public static init = () => {
+        if (!this.config)
+            throw Error("Command Manager initialized before configured.");
+        server.world.beforeEvents.chatSend.subscribe(this.chatSendBefore);
+    };
 }
 
 type CommandManagerConfigData = {
-	prefix: string;
+    prefix: string;
 };
